de19dc64f3d894d2f3a7b47fe99bba8a4a939cb3
Andreas Edvardsen <andreas.edvardsen@abax.no>
2017-12-06 11:20:27 +0100

    init

---
+++ LeaderboardWebApi/LeaderboardWebApi/Controllers/NewUserController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaderboardWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/NewUser")]
+    public class NewUserController : Controller
+    {
+    }
+}
